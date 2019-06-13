3efc85675a8dd662d194ac38266a0e229902a22a
Mikko Pitkänen <mikko.pitkanen@tuni.fi>
2019-06-13 12:45:44 +0300

    Implement all pthread functions used by Kvazaar

--- ThreadWrapper/main.cpp
+++ ThreadWrapper/main.cpp
@@ -1,7 +1,26 @@
 
 #include "threads.h"
+#include <iostream>
+#include <string>
+#include <thread>
 
+
+void* f(void* a) {
+    int i = *(int*)a;
+    while (i) {
+        std::cout << std::string(std::to_string(i--) + '\n');
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    return nullptr;
+}
 
 int main(int argc, char** argv) {
+    pthread_t t1, t2, t3;
+    int i1 = 3, i2 = 4, i3 = 2;
+    pthread_create(&t1, nullptr, f, &i1);
+    pthread_create(&t2, nullptr, f, &i2);
+    pthread_create(&t3, nullptr, f, &i3);
+    pthread_join(t2, nullptr);
+    std::cout << "f exit" << std::endl;
     return 0;
 }
--- ThreadWrapper/threads.h
+++ ThreadWrapper/threads.h
@@ -4,15 +4,18 @@
 extern "C" {
 #endif
 
-using pthread_t = unsigned int;
+using pthread_cond_t = void*;
+using pthread_mutex_t = void*;
+using pthread_t = void*;
 using voidp_voidp_func = void*(*)(void*);
 
 using placeholder = void;
 using pthread_attr_t = placeholder;
-using pthread_mutex_t = placeholder;
-using pthread_cond_t = placeholder;
-using pthread_mutexattr_t = placeholder;
 using pthread_condattr_t = placeholder;
+using pthread_mutexattr_t = placeholder;
+
+// Parameter names that have been commented away do nothing,
+// as they are always null when the functions are used in Kvazaar.
 
 int pthread_cond_broadcast(pthread_cond_t* cond);
 int pthread_cond_destroy(pthread_cond_t* cond);
@@ -20,7 +23,7 @@ int pthread_cond_init(pthread_cond_t* cond, const pthread_condattr_t* /*attr*/);
 int pthread_cond_signal(pthread_cond_t* cond);
 int pthread_cond_wait(pthread_cond_t* cond, pthread_mutex_t* mutex);
 
-int pthread_create(pthread_t* thread, const pthread_attr_t* /*attr*/, voidp_voidp_func start_routine, void* arg);
+int pthread_create(pthread_t* thread, const pthread_attr_t* /*attr*/, voidp_voidp_func executee, void* arg);
 void pthread_exit(void* /*value_ptr*/);
 int pthread_join(pthread_t thread, void** /*value_ptr*/);
 
